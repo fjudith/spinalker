eafa3f51a13d4f304b66994dfa61ac425028a805
Florian JUDITH <florian.judith.b@gmail.com>
2021-03-11 13:55:10 +0100

    spinal-organ-api-gateway

---
+++ build/spinal-organ-api-gateway.sh
@@ -0,0 +1,4 @@
+docker build \
+-f ./containers/spinal-organ-api-gateway/Dockerfile \
+-t fjudith/spinal-organ-api-gateway \
+./containers/spinal-organ-api-gateway/
---
+++ hack/spinal-organ-api-gateway.sh
@@ -0,0 +1,4 @@
+docker container run \
+--rm \
+--name spinal-organ-api-gateway \
+fjudith/spinal-organ-api-gateway
