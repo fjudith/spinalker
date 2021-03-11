4d4f66a3d666fc2cfce7cf6434978aaa46125cf6
Florian JUDITH <florian.judith.b@gmail.com>
2021-03-11 13:16:38 +0100

    Added plugin-graph_viewer

---
+++ build/spinal-env-viewer-plugin-graph_viewer.sh
@@ -0,0 +1,4 @@
+docker build \
+-f ./containers/spinal-env-viewer-plugin-graph_viewer/Dockerfile \
+-t fjudith/spinal-env-viewer-plugin-graph_viewer \
+./containers/spinal-env-viewer-plugin-graph_viewer/
---
+++ hack/spinal-env-viewer-plugin-graph_viewer.sh
@@ -0,0 +1,4 @@
+docker container run \
+--rm \
+--name spinal-env-viewer-plugin-graph_viewer \
+fjudith/spinal-env-viewer-plugin-graph_viewer
