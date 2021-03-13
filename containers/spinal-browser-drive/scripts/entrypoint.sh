54e3168f91edba6d2447eedcb418b54f9403629d
Florian JUDITH <florian.judith.b@gmail.com>
2021-03-14 00:08:42 +0100

    Added pm2 runtime container

---
+++ containers/spinal-browser-drive/scripts/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# Setting default environnement variables values
+# -----------------------------------------------
+SPINALHUB_PORT=${SPINALHUB_PORT:-"8888"}
+SPINALHUB_HOST=${SPINALHUB_HOST:-"localhost"}
+
+SPINAL_USER_ID=${SPINAL_USER_ID:-"168"}
+SPINAL_PASSWORD=${SPINAL_PASSWORD:-"6Y6Wv8nuJ"}
+SPINAL_PASSWORD_ROOT=${SPINAL_PASSWORD_ROOT:-"uD24R2eeC"}
+SPINAL_PASSWORD_USER=${SPINAL_PASSWORD_USER:-"jk57gZ3RE"}
+
+# Build configuration file, only if not mounted
+# -----------------------------------------------
+if ! $(ls ${APP_PATH}/spinal-browser-drive/.config.json); then
+    cp /tmp/.config.json.tpl ${APP_PATH}/spinal-browser-drive/.config.json && \
+    sed -i "s/__SPINALHUB_PORT__/${SPINALHUB_PORT}/g" ${APP_PATH}/spinal-browser-drive/.config.json
+    sed -i "s/__SPINALHUB_IP__/${SPINALHUB_HOST}/g" ${APP_PATH}/spinal-browser-drive/.config.json
+    sed -i "s/SPINAL_USER_ID/${SPINAL_USER_ID}/g" ${APP_PATH}/spinal-browser-drive/.config.json
+    sed -i "s/SPINAL_PASSWORD/${SPINAL_PASSWORD}/g" ${APP_PATH}/spinal-browser-drive/.config.json
+    sed -i "s/SPINAL_PASSWORD_ROOT/${SPINAL_PASSWORD_ROOT}/g" ${APP_PATH}/spinal-browser-drive/.config.json
+    sed -i "s/SPINAL_PASSWORD_USER/${SPINAL_PASSWORD_USER}/g" ${APP_PATH}/spinal-browser-drive/.config.json
+fi
+
+exec "$@"
