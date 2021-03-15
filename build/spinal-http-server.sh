14b2809cceee24db42d348911ab01e7e73667bb4
Florian JUDITH <florian.judith.b@gmail.com>
2021-03-15 01:53:38 +0100

    Add files directory copy

--- build/spinal-http-server.sh
+++ build/spinal-http-server.sh
@@ -1,8 +1,10 @@
 cp -vr ./containers/spinal-http-server/templates ./src/spinal-http-server/ && \
 cp -vr ./containers/spinal-http-server/scripts ./src/spinal-http-server/ && \
+cp -vr ./containers/spinal-http-server/files ./src/spinal-http-server/ && \
 docker build \
 -f ./containers/spinal-http-server/Dockerfile \
 -t fjudith/spinal-http-server \
 ./src/spinal-http-server/ && \
-rm -vrf ./src/spinal-http-server/templates
-rm -vrf ./src/spinal-http-server/scripts
+rm -vrf ./src/spinal-http-server/templates && \
+rm -vrf ./src/spinal-http-server/scripts && \
+rm -vrf ./src/spinal-http-server/files
--- build/spinal-organ-api-gateway.sh
+++ build/spinal-organ-api-gateway.sh
@@ -1,8 +1,10 @@
 cp -vr ./containers/spinal-organ-api-gateway/templates ./src/spinal-organ-api-gateway/ && \
 cp -vr ./containers/spinal-organ-api-gateway/scripts ./src/spinal-organ-api-gateway/ && \
+cp -vr ./containers/spinal-organ-api-gateway/files ./src/spinal-organ-api-gateway/ && \
 docker build \
 -f ./containers/spinal-organ-api-gateway/Dockerfile \
 -t fjudith/spinal-organ-api-gateway \
 ./src/spinal-organ-api-gateway/ && \
-rm -vrf ./src/spinal-organ-api-gateway/templates
-rm -vrf ./src/spinal-organ-api-gateway/scripts
+rm -vrf ./src/spinal-organ-api-gateway/templates && \
+rm -vrf ./src/spinal-organ-api-gateway/scripts && \
+rm -vrf ./src/spinal-organ-api-gateway/files
