f5392bc262f40dec226919bea5159bff6b561168
Florian JUDITH <florian.judith.b@gmail.com>
2021-03-16 00:04:00 +0100

    #8 Added env-admin workflow
    * Include required project for #7

---
+++ build/spinal-env-admin-panel-hub-status.sh
@@ -0,0 +1,4 @@
+docker build \
+-f ./containers/spinal-env-admin-panel-hub-status/Dockerfile \
+-t fjudith/spinal-env-admin-panel-hub-status \
+./containers/spinal-env-admin-panel-hub-status/
---
+++ build/spinal-env-admin-panel-user-manager.sh
@@ -0,0 +1,4 @@
+docker build \
+-f ./containers/spinal-env-admin-panel-user-manager/Dockerfile \
+-t fjudith/spinal-env-admin-panel-user-manager \
+./containers/spinal-env-admin-panel-user-manager/
