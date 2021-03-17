3f7c394d97c6849984790e844329a8a877529ce8
Florian JUDITH <florian.judith.b@gmail.com>
2021-03-17 04:52:19 +0100

    #9 Added spinal-http-server running in caddy

---
+++ containers/spinal-caddy-server/scripts/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+set -ex
+
+# Setting default environnement variables values
+# -----------------------------------------------
+SPINALHUB_PORT=${SPINALHUB_PORT:-"8888"}
+SPINALHUB_HOST=${SPINALHUB_HOST:-"localhost"}
+SPINAL_USER_ID=${SPINAL_USER_ID:-"168"}
+SPINAL_PASSWORD=${SPINAL_PASSWORD:-"6Y6Wv8nuJ"}
+SPINAL_PASSWORD_ROOT=${SPINAL_PASSWORD_ROOT:-"uD24R2eeC"}
+SPINAL_PASSWORD_USER=${SPINAL_PASSWORD_USER:-"jk57gZ3RE"}
+
+# Build configuration file, only if not mounted
+# -----------------------------------------------
+if ! ls ${APP_PATH}/spinal-http-server/.config.json; then
+    cp /tmp/.config.json.tpl ${APP_PATH}/spinal-http-server/.config.json && \
+    sed -i "s/__SPINALHUB_PORT__/${SPINALHUB_PORT}/g" ${APP_PATH}/spinal-http-server/.config.json
+    sed -i "s/__SPINALHUB_HOST__/${SPINALHUB_HOST}/g" ${APP_PATH}/spinal-http-server/.config.json
+    sed -i "s/__SPINAL_USER_ID__/${SPINAL_USER_ID}/g" ${APP_PATH}/spinal-http-server/.config.json
+    sed -i "s/__SPINAL_PASSWORD__/${SPINAL_PASSWORD}/g" ${APP_PATH}/spinal-http-server/.config.json
+    sed -i "s/__SPINAL_PASSWORD_ROOT__/${SPINAL_PASSWORD_ROOT}/g" ${APP_PATH}/spinal-http-server/.config.json
+    sed -i "s/__SPINAL_PASSWORD_USER__/${SPINAL_PASSWORD_USER}/g" ${APP_PATH}/spinal-http-server/.config.json
+fi
+
+exec "$@"
---
+++ src/spinal-caddy-server/index.js
@@ -0,0 +1,38 @@
+const path = require('path');
+// changer le path et port ici
+const absPath = './html/'.split('/');
+const root = path.join(__dirname, ...absPath);
+const portHub = process.env.SPINALHUB_PORT || 7080;
+const HUB_HOST = `http://${process.env.SPINALHUB_HOST}:${portHub}` || `http://localhost:${portHub}`; // modif here
+const port = parseInt(portHub, 10) + 4;
+
+var proxy = require('express-http-proxy');
+var express = require('express');
+var app = express();
+const proxyHub = proxy(HUB_HOST, {
+  limit: '1tb',
+  proxyReqPathResolver: function (req) {
+    return req.originalUrl;
+  }
+});
+
+const routesToProxy = [
+  '/sceen',
+  '/get_user_id',
+  '/get_admin_id',
+  '/get_new_account',
+  '/get_confirm_new_account',
+  '/get_resend_confirmation',
+  '/get_new_password',
+  '/get_change_user_password',
+  '/get_delete_account',
+  '/get_change_user_password_by_admin',
+  '/get_delete_account_by_admin',
+  '/get_change_account_rights_by_admin'
+];
+for (const routeToProxy of routesToProxy) {
+  app.use(routeToProxy, proxyHub);
+}
+app.use(express.static(root));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
