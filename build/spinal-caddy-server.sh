bf47c9f9ce43ba100f8d3b4106740fbc734862a8
Florian JUDITH <florian.judith.b@gmail.com>
2021-03-18 02:02:06 +0100

    Fixed missing required build directory

--- build/spinal-caddy-server.sh
+++ build/spinal-caddy-server.sh
@@ -1,8 +1,10 @@
 cp -vr ./containers/spinal-caddy-server/templates ./src/spinal-caddy-server/ && \
 cp -vr ./containers/spinal-caddy-server/scripts ./src/spinal-caddy-server/ && \
+cp -vr ./containers/spinal-caddy-server/files ./src/spinal-caddy-server/ && \
 docker build \
 -f ./containers/spinal-caddy-server/Dockerfile \
 -t fjudith/spinal-caddy-server \
 ./src/spinal-caddy-server/ && \
 rm -vrf ./src/spinal-caddy-server/templates && \
-rm -vrf ./src/spinal-caddy-server/scripts
+rm -vrf ./src/spinal-caddy-server/scripts && \
+rm -vrf ./src/spinal-caddy-server/files
