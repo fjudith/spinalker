9517beae9e882767075dff20929bc95c39c1d22b
Florian JUDITH <florian.judith.b@gmail.com>
2021-03-24 00:47:14 +0100

    #16 spinal-dataset-provisioner container/job

---
+++ build/spinal-dataset-provisioner.sh
@@ -0,0 +1,4 @@
+docker build \
+-f ./containers/spinal-dataset-provisioner/Dockerfile \
+-t fjudith/spinal-dataset-provisioner \
+./containers/spinal-dataset-provisioner/
--- build/spinal-dataset.sh
+++
@@ -1,4 +0,0 @@
-docker build \
--f ./containers/spinal-core-hub-dataset/Dockerfile \
--t fjudith/spinal-core-hub-dataset \
-./containers/spinal-core-hub-dataset/
