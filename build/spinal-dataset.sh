530e2e3eed9362d36194a178d9df8c461be8bcd6
Florian JUDITH <florian.judith.b@gmail.com>
2021-03-23 23:12:28 +0100

    Added dataset importer

---
+++ build/spinal-dataset.sh
@@ -0,0 +1,4 @@
+docker build \
+-f ./containers/spinal-core-hub-dataset/Dockerfile \
+-t fjudith/spinal-core-hub-dataset \
+./containers/spinal-core-hub-dataset/
---
+++ containers/spinal-core-hub-dataset/scripts/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -e
+
+MINIO_ENABLED=${USER_MINIO:-"true"}
+S3CMD_ENABLED=${S3CMD_ENABLED:-"false"}
+S3_HOST=${S3_HOST:-"s3.fr-par.scw.cloud"}
+S3_BUCKET=${S3_BUCKET:-"connect"}
+S3_REGION=${S3_REGION:-"fr-par"}
+S3_ACCESSKEY=${S3_ACCESSKEY:-"null"}
+S3_SECRETKEY=${S3_SECRETKEY:-"null"}
+SPINAL_ARCHIVE=${SPINAL_ARCHIVE:-"memory.tar.gz"}
+
+if [ "$MINIO_ENABLED" == "true" ]; then
+    echo "Dowloading archive using minio"
+    mc alias set objstore "https://${S3_HOST}" "${S3_ACCESSKEY}" "${S3_SECRETKEY}" --api S3v4
+    mc cp objstore/${S3_BUCKET}/${SPINAL_ARCHIVE} ${APP_PATH}/memory/
+elif [ "$S3CMD_ENABLED" == "true" ]; then
+    echo "Downloading archive using s3cmd"
+cat <<EOF > ~/.s3cfg
+[default]
+# Object Storage Region "${S3_REGION}"
+host_base = ${S3_HOST}
+host_bucket = %{S3_BUCKET}.${S3_HOST}
+bucket_location = ${S3_REGION}
+use_https = True
+
+# Login credentials
+access_key = ${S3_ACCESSKEY}
+secret_key = ${S3_SECRETKEY}
+EOF
+    s3cmd get s3://${S3_BUCKET}/${SPINAL_ARCHIVE} ${APP_PATH}/memory/
+fi
+
+cd ${APP_PATH}/memory/
+
+echo "Extracting archive"
+tar xvzf ${SPINAL_ARCHIVE}
+
+echo "Updating permission"
+chown -R ${USER}:${GROUP} ${APP_PATH}/memory/
+
+echo "Remove downloaded archive"
+rm -vf ${SPINAL_ARCHIVE}
+
+exec "$@"
